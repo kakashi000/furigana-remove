e10c30985ac8d0027b81ad87bc6580f10f5d010f
Seth A <rrk061295@gmail.com>
2020-06-20 10:25:36 +0800

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,59 @@
+'use strict'
+
+module.exports = {
+  siteMetadata: {
+    title: 'furigana-remove',
+    description: 'A tool for removing furigana from subtitle files.',
+    keywords: 'japanese, furigana, subtitles',
+    siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com',
+    author: {
+      name: 'kakashi000',
+      url: 'https://github.com/kakashi000'
+    }
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/src/content`
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1rem'
+            }
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1140,
+              quality: 90,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    },
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-plugin-canonical-urls',
+      options: {
+        siteUrl: 'https://gatsby-starter-typescript-plus.netlify.com'
+      }
+    },
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet'
+  ]
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react'
+
+import Dropzone from 'react-dropzone'
+import styled from '@emotion/styled'
+
+import JSZip from 'jszip'
+import { saveAs } from 'file-saver'
+
+import Page from '../components/Page'
+import Container from '../components/Container'
+import IndexLayout from '../layouts'
+
+async function handleFiles(files: File[]): Promise<void> {
+  const texts = await Promise.all(files.map(file => file.text()))
+  const newTexts = texts.map(text => text.replace(/[（(][ぁ-ん]*[)）]/, ''))
+  const zip = new JSZip()
+  newTexts.forEach((text, index) => {
+    zip.file(files[index].name, text)
+  })
+  const content = await zip.generateAsync({ type: 'blob' })
+  saveAs(content, 'output.zip')
+}
+
+const DropzoneContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  border-width: 2px;
+  border-radius: 2px;
+  border-style: dashed;
+  background-color: #fafafa;
+  color: #bdbdbd;
+  outline: none;
+  transition: border 0.24s ease-in-out;
+`
+
+const IndexPage = () => (
+  <IndexLayout>
+    <Page>
+      <Container>
+        <h1>ようこそ</h1>
+        <p>Upload some subtitle files, this tool will remove furigana from each one and pack everything into a zip file.</p>
+        <Dropzone onDrop={handleFiles}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <DropzoneContainer {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag and drop some files here, or click to select files</p>
+              </DropzoneContainer>
+            </section>
+          )}
+        </Dropzone>
+      </Container>
+    </Page>
+  </IndexLayout>
+)
+
+export default IndexPage
